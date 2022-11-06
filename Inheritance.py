7731c6fd8500f66de931290e446dff5dd3944bb0
Ambarish Bhagawati <ambarish8653@gmail.com>
2022-11-06 11:37:16 +0530

    Inheritance edited

--- Inheritance.py
+++ Inheritance.py
@@ -3,13 +3,13 @@ class Father:
     def method1(self):
         print("Method 1 working")
 
-class Son(Father):
-
+class Son(Father):  # Father is inherited by Son
+    
     def method2(self):
         print("Method 2 working")
 
 obj=Father()
 obj.method1()
 
-obj2=Son()
+obj2=Son()  # Son can use both method1 and method2 because of Inheritance.
 obj2.method2()
